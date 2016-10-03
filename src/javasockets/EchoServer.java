0350ddd825f82ff9a1c172f3a8cb7e5989f34e3a
Sara <ogaz@stsci.edu>
2016-10-03 18:04:15 -0400

    adding echo java programs, and a gitignore

---
+++ src/javasockets/EchoServer.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.io.PrintWriter;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+/**
+ *
+ * @author ogaz
+ */
+public class EchoServer {
+    
+        public static void main(String[] args) throws IOException{
+        
+            if (args.length != 1) {
+                System.err.println("Usage: java EchoServer <port number>");
+                System.exit(1);
+            }
+            
+            int portNumber = Integer.parseInt(args[0]);
+            
+            try (
+                    ServerSocket serverSocket = 
+                            new ServerSocket (Integer.parseInt(args[0]));
+                    Socket clientSocket = serverSocket.accept();
+                    PrintWriter out =
+                            new PrintWriter(clientSocket.getOutputStream(), true);
+                    BufferedReader in = new BufferedReader(
+                        new InputStreamReader(clientSocket.getInputStream()))
+           ) {
+                String inputLine;
+                while ((inputLine = in.readLine()) != null) {
+                    out.println(inputLine);
+                }
+            } catch (IOException e) {
+                System.out.println("Exception caught when trying to listen on port "
+                    + portNumber + " or listening for a connection");
+                System.out.println(e.getMessage());
+            }
+    }    
+}
